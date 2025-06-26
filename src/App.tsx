cdbadb7efa37cbbba897d81c9c6a80c93b7653b9
Alex <dzekovalex@gmail.com>
2025-06-26 17:14:54 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className='App bg-gray-800 min-h-screen flex items-center justify-center'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
