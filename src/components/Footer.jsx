22ca8b8d4a0a1962a4f0d577244857a976b6c566
Alex <alexander.zhukov@tptlive.ee>
2025-06-30 14:11:13 +0300

    feat: Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="px-5 py-4 bg-blue-700 border-gray-400 border-t-3 mt-5 rounded-t-2xl shadow-2xl text-center">
+      <p>© 2025 My Website</p>
+    </footer>
+  );
+}
