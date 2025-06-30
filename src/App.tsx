ef45039a78db8859bd656ec1923e4c08351cdb1d
Alex <alexander.zhukov@tptlive.ee>
2025-06-30 14:12:01 +0300

    feat: Added components on main paeg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <div className='App bg-gray-800 min-h-screen flex items-center justify-center'>
-
+      <div className='flex items-center justify-center min-h-screen'>
+        <div className='absolute top-0 flex justify-center w-full'>
+          <Header />
+        </div>
+        <main className="w-full flex flex-col items-center">
+          <Hero />
+          
+        </main>
       </div>
+     <Footer />
     </>
   )
 }
