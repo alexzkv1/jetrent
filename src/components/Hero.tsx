1f7bd7498b0f008e8e9716627cde1bd0866fecd9
Alex <alexander.zhukov@tptlive.ee>
2025-07-22 21:17:17 +0300

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <div className="max-w-7xl hero p-6 flex flex-col md:flex-row rounded-2xl mx-auto shadow-2xl bg-blue-800 border-3 border-gray-200">
         
         <img
-            src="./images/wave.jpg"
+            src="./wave.jpg"
             alt="Jetski"
             className=" w-250 h-100 object-cover rounded-lg shadow-lg mb-6 md:mb-0 border-2"
         />
