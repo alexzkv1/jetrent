df9afed0a6c751e5531faebf40f1731c258fc16e
Alex <alexander.zhukov@tptlive.ee>
2025-07-22 20:51:03 +0300

    upd: Relocated images

--- src/pages/Jets.tsx
+++ src/pages/Jets.tsx
@@ -11,9 +11,9 @@ export default function Jets() {
             <p className="text-lg w-full">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc bibendum, mi at bibendum scelerisque, est ligula pellentesque turpis, nec placerat nunc dui non dui.</p>
         </div>
-        <JetCard image={'GTI.jpg'} title={'Seadoo GTI-130'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
-        <JetCard image={'Yamaha.jpg'} title={'Yamaha'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
-        <JetCard image={'Spark.jpg'} title={'Seadoo Spark Trixx'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
+        <JetCard image={'../images/GTI.jpg'} title={'Seadoo GTI-130'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
+        <JetCard image={'../images/Yamaha.jpg'} title={'Yamaha'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
+        <JetCard image={'../images/Spark.jpg'} title={'Seadoo Spark Trixx'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisi in libero dictum pharetra. Nulla facilisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent facilisis egestas leo non eleifend. Suspendisse mollis lectus a lorem.'} />
         </div>
     )
 };
