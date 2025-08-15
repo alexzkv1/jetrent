57e4b30765d2e3c4fec9389df05bcfd4ca3a5b4a
Alex <alexander.zhukov@tptlive.ee>
2025-08-15 16:26:21 +0300

    feat: added animations and on scroll event for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,31 @@
 import Menu from './Menu';
 import { Link } from 'react-router-dom';
+import {useState, useEffect } from 'react';
+import { motion } from 'motion/react';
 
 export default function Header() {
+    const [scrolled, setScrolled] = useState(false);
+    const transition = {
+        duration: 1.5,
+        delay: 0.2,
+        ease: [0, 0.71, 0.2, 1.01],
+    }
+    useEffect(() => {
+        const onScroll = () => setScrolled(window.scrollY > 100);
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrolled])
+
     return(
-    <div className="flex justify-center z-10 relative px-2 sm:px-0">
-        <div className="w-full py-4 md:max-w-350 bg-blue-700 fixed border-gray-200 border-3 rounded-2xl shadow-2xl">
+    <motion.div  initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={transition} 
+    className="flex justify-center z-10 relative px-2 sm:px-0">
+        <div className={`lg:max-w-350 fixed w-full transition-colors duration-300 py-4 lg:rounded-2xl ${
+        scrolled ? "bg-blue-800 border-2 border-gray-200" : "bg-transparent shadow-2xl"}`}>
             <div className="flex items-center justify-between px-2 sm:px-4 md:px-5 lg:px-5">
                 <Menu />
-               <Link to='/jetrent'><img src='./logo.svg' alt='Logo' className='w-16 h-16 lg:w-18 lg:h-18' /></Link>
-                <Link to="/broneeri" className="btn relative overflow-hidden rounded-lg border-2 bg-blue-900 border-gray-400 lg:text-2xl
+               <Link to='/'><motion.img whileHover={{ scale: 1.2 }} transition={{ duration: 0.2, ease: "easeOut" }} src='./logo.svg' alt='Logo' className='w-16 h-16 lg:w-18 lg:h-18' /></Link>
+                <Link to="/broneeri">
+                <motion.button whileHover={{ scale: 1.15 }} transition={{ duration: 0.2, ease: "easeOut" }} className="btn relative overflow-hidden rounded-lg border-2 bg-blue-900 border-gray-400 lg:text-2xl
                 font-bold text-white  transition-colors duration-300 group text-xl py-6 px-1 sm:px-3 md:px-4 lg:py-8 lg:px-5">
                     <span className="relative z-10">Broneeri</span>
                     <span className="pointer-events-none  left-0 bottom-0 w-full h-full">
@@ -35,10 +52,11 @@ export default function Header() {
                         </path>
                         </svg>
                     </span>
+                </motion.button>
                 </Link>
             </div>
         </div>
-        </div>
+        </motion.div>
         
     )
 }
