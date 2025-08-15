bf05daeb43b726660e90363c819c09ddf9f93790
Alex <alexander.zhukov@tptlive.ee>
2025-08-15 16:25:53 +0300

    feat: added animations for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
+import { motion } from "motion/react";
+
 export default function Footer() {
   return (
-<footer className="footer w-full min-w-0 p-4 border-t-3 bg-blue-700 flex justify-center">
+  <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="footer w-full p-4 border-t-3 bg-blue-700 flex justify-center">
     <p className="text-md">JetRent © {new Date().getFullYear()} - All right reserved</p>
-</footer>
+  </motion.div>
   );
 }
